fce4c40f52057ef4941e8880a799a9bfc36f4dd8
Nathan Mazzapica <natemaz513@gmail.com>
2026-07-09 10:58:51 -0400

    feat: add err display to display

--- display/display.py
+++ display/display.py
@@ -12,7 +12,11 @@ class Display():
         if lux < 100_000:
             return f"{k_lux:.1f}K"
 
-        return f"{k_lux:.0f}K"
+        if lux < 1_000_000:
+            return f"{k_lux:.0f}K"
+
+        m_lux = lux / 1_000_000
+        return f"{m_lux:.1f}M"
 
     def render(self, state):
         self.LCD.write_line(f"Lux:{self._format_lux(state.lux_seconds)}s|M:{state.moisture:.0f}%", 0)
@@ -21,3 +25,8 @@ class Display():
     
     def write(self, body):
         self.LCD.write_line(str(body), 0)
+
+    def display_err(self, desc: str, errno: int):
+        self.LCD.write_line(f"Err[{str(errno)}]")
+        self.LCD.marquee_text(desc, 1)
+
